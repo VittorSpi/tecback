32c5b16ca68e3a128b184a29ab3441fe03876efb
VittorSpi <144367677+VittorSpi@users.noreply.github.com>
2024-09-10 23:38:42 -0300

    -feat Adicionando DTO e Repository

---
+++ src/main/java/br/com/fujideia/iesp/tecback/repository/AtorRepository.java
@@ -0,0 +1,19 @@
+package br.com.fujideia.iesp.tecback.repository;
+
+import br.com.fujideia.iesp.tecback.model.Ator;
+import br.com.fujideia.iesp.tecback.model.Filme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AtorRepository extends JpaRepository<Ator, Long> {
+    @Query("SELECT a FROM Ator a WHERE a.nome LIKE %:nome%")
+    List<Ator> buscarPorNome(@Param("nome") String nome);
+
+    @Query("SELECT a FROM Ator a JOIN a.filmes f WHERE f IN :filmes")
+    List<Ator> buscarPorFilme(@Param("filmes")List<Filme> filmes);
+}
